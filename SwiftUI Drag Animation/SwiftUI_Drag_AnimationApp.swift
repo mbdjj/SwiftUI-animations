9e92aca23011dcb484b8fd872d2ecac799cd398d
Marcin Bartmiński <mbdjj@o2.pl>
2022-12-10 20:19:54 +0100

    Initial Commit

---
+++ SwiftUI Drag Animation/SwiftUI_Drag_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Drag_AnimationApp.swift
+//  SwiftUI Drag Animation
+//
+//  Created by Marcin Bartminski on 10/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Drag_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
